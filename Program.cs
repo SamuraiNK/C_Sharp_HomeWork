2268dfacb981e24e8fdad5d18068401bf54aa3b4
Samurai <samurai230613@gmail.com>
2023-07-08 01:07:58 +0300

    Final version

--- Program.cs
+++ Program.cs
@@ -1,68 +1,68 @@
-﻿// // Задание 2:
+﻿ //Задание 2:
 
-// Console.WriteLine ("Я програма которая умеет из 2 чисел называть наибольшее");
-// Console.WriteLine ("Давай попробуем, напиши первое число");
-// int number_1 = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine ("Я програма которая умеет из 2 чисел называть наибольшее");
+Console.WriteLine ("Давай попробуем, напиши первое число");
+int number_1 = Convert.ToInt32 (Console.ReadLine());
 
-// Console.WriteLine ("Отлично а теперь введи второе");
-// int number_2 = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine ("Отлично а теперь введи второе");
+int number_2 = Convert.ToInt32 (Console.ReadLine());
  
-// if (number_1 > number_2)
-// {
-//     Console.WriteLine ("Это число");
-//     Console.Write (number_1 );
-// }
-// if (number_1 < number_2)
-// {
-//     Console.WriteLine ("Это число");
-//     Console.Write (number_2);
-// }
+if (number_1 > number_2)
+{
+    Console.WriteLine ("Это число");
+    Console.Write (number_1 );
+}
+if (number_1 < number_2)
+{
+    Console.WriteLine ("Это число");
+    Console.WriteLine (number_2);
+}
 
 
 
 
 // Задание 4:
 
-// Console.WriteLine ("Приевет я стала умнее");
-// Console.WriteLine ("Теперь я могу назвать наибольшее число из 3 чисел");
+Console.WriteLine ("Я стала умнее");
+Console.WriteLine ("Теперь я могу назвать наибольшее из 3 чисел");
 
-// Console.WriteLine ("Напиши первое число");
-// int a = Convert.ToInt32 (Console.ReadLine ());
+Console.WriteLine ("Напиши первое число");
+int a = Convert.ToInt32 (Console.ReadLine ());
 
-// Console.WriteLine ("Напиши второе число");
-// int b = Convert.ToInt32 (Console.ReadLine ());
+Console.WriteLine ("Напиши второе число");
+int b = Convert.ToInt32 (Console.ReadLine ());
 
-// Console.WriteLine ("Напиши третье число");
-// int c = Convert.ToInt32 (Console.ReadLine ());
+Console.WriteLine ("Напиши третье число");
+int c = Convert.ToInt32 (Console.ReadLine ());
 
-// int max_num = 0;
+int max_num = 0;
 
-// if (a > b)
-// {
-//     max_num = a;
-// }
-// if (a < b)
-// {
-//     max_num = b;
-// }
-// if (max_num < c)
-// {
-//     max_num = c;
-// }
-// Console.WriteLine (max_num);
-
-
+if (a > b)
+{
+    max_num = a;
+}
+if (a < b)
+{
+    max_num = b;
+}
+if (max_num < c)
+{
+    max_num = c;
+}
+Console.WriteLine (max_num);
 
 
 
-// Задача 6:
 
-Console.WriteLine ("Теперь я могу говорить какое число чётное а какое нет");
+
+// // Задача 6:
+
+Console.WriteLine ("Теперь я могу говорить какое число чётное, а какое нет");
 
 Console.WriteLine ("Напиши любое число");
-double a = Convert.ToInt32 (Console.ReadLine ());
+double result = Convert.ToInt32 (Console.ReadLine ());
 
-if (a % 2 > 0 )
+if (result % 2 > 0 )
 {
     Console.WriteLine ("Оно нечётное");
 }
@@ -73,3 +73,19 @@ else
 
 
 
+
+
+// Задача 8
+
+Console.WriteLine ("А теперь что нибдуь послжнее");
+Console.WriteLine ("Я назову все чётные от одного до числа которое ты назовёшь");
+Console.WriteLine ("Напиши любое число");
+
+int x = Convert.ToInt32 (Console.ReadLine ());
+int i = 0;
+
+while (i < x)
+{
+    Console.WriteLine (i);
+    i += 2;   
+}
