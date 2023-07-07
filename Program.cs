d49fb13d6bea7914f99feab5f3d831944ba9878e
Samurai <samurai230613@gmail.com>
2023-07-08 00:00:47 +0300

    First task

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// Задание 2:
+
+Console.WriteLine ("Я програма которая умеет из 2 чисел называть наибольшее");
+Console.WriteLine ("Давай попробуем, напиши первое число");
+int number_1 = Convert.ToInt32 (Console.ReadLine());
+
+Console.WriteLine ("Отлично а теперь введи второе");
+int number_2 = Convert.ToInt32 (Console.ReadLine());
+ 
+if (number_1 > number_2)
+{
+    Console.WriteLine ("Это число");
+    Console.Write (number_1 );
+}
+if (number_1 < number_2)
+{
+    Console.WriteLine ("Это число");
+    Console.Write (number_2);
+}
+
+
+
+
