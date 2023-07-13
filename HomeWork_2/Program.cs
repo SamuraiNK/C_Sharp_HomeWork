de6602994b65e7aa9f6c91622e436acb4d183c81
Samurai <samurai230613@gmail.com>
2023-07-13 15:58:50 +0300

    Final commit HomeWork_2

--- HomeWork_2/Program.cs
+++ HomeWork_2/Program.cs
@@ -1,32 +1,32 @@
 ﻿// Задача 10
 
-// Console.WriteLine ("Привет если ты введёшь трёхзначное число я смогу показать тебе вторую цифру этого числа");
-// Console.WriteLine ("Введи число ");
+ Console.WriteLine ("Привет если ты введёшь трёхзначное число я смогу показать тебе вторую цифру этого числа");
+ Console.WriteLine ("Введи число ");
 
-// int result = Convert.ToInt32 (Console.ReadLine ());
+ int result = Convert.ToInt32 (Console.ReadLine ());
 
-// result = result / 10;
-// result = result % 10;
+ result = result / 10;
+ result = result % 10;
 
 
-// Console.WriteLine ($"Это число  {result}");
+ Console.WriteLine ($"Это число  {result}");
 
 
 // Задача 13
 
-// Console.WriteLine ("Теперь я назову третию цифру");
-// Console.WriteLine ("Введи число ");
+ Console.WriteLine ("Теперь я назову третию цифру");
+ Console.WriteLine ("Введи число ");
 
-// int result_2 = Convert.ToInt32 (Console.ReadLine());
-// if (result_2 < 100)
-// {
-//     Console.WriteLine ($"Это число 2-х значное число а надо 3-х значное");
-// }
-// else
-// {
-//     result_2 = result_2 % 10;
-//     Console.WriteLine ($"Это число {result_2}");
-// }
+ int result_2 = Convert.ToInt32 (Console.ReadLine());
+ if (result_2 < 100)
+ {
+     Console.WriteLine ($"Это число 2-х значное число а надо 3-х значное");
+ }
+ else
+ {
+    result_2 = result_2 % 10;
+    Console.WriteLine ($"Это число {result_2}");
+ }
 
 
 
