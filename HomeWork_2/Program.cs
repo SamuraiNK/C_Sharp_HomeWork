b35a7f0a90efb2bbec9821660bb09396d6058f74
Samurai <samurai230613@gmail.com>
2023-07-13 15:17:05 +0300

    First quest

--- HomeWork_2/Program.cs
+++ HomeWork_2/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10
+
+Console.WriteLine ("Привет если ты введёшь трёхзначное число я смогу показать тебе вторую цифру этого числа");
+Console.WriteLine ("Введи число ");
+
+int result = Convert.ToInt32 (Console.ReadLine ());
+
+result = result / 10;
+result = result % 10;
+
+
+Console.WriteLine ($"Это число  {result}");
+
