5d3c4bd4b4d1dbb907310eaf0c130e910d888d47
Samurai <samurai230613@gmail.com>
2023-07-21 14:38:13 +0300

    new console for HomeWork_4

---
+++ HomeWork_4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два 
+// числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+
+Console.WriteLine("Введите число которое будете возводит");
+int numA = Convert.Toint32(Console.ReadLine());
+
+Console.WriteLine("Введите степень число ");
+int numB = Convert.Toint32(console.ReadLine());
