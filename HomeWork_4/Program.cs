f6c9df2d013d8551938fb8c82fdf9fbbfaf5eee4
Samurai <samurai230613@gmail.com>
2023-07-21 16:19:20 +0300

    Final quest if HomeWork_4

--- HomeWork_4/Program.cs
+++ HomeWork_4/Program.cs
@@ -4,21 +4,21 @@
 // 2, 4 -> 16
 
 
-// Console.WriteLine("Введите число которое будут возводить в степень");
-// int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число которое будут возводить в степень");
+int numA = Convert.ToInt32(Console.ReadLine());
 
 
-// Console.WriteLine("Введи степень");
-// int numB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи степень");
+int numB = Convert.ToInt32(Console.ReadLine());
 
-// int result = numA;
+int result = numA;
 
-// for (int i = 1; i < numB; i++)
-// {
-//     result = result * numA;
-// }
+for (int i = 1; i < numB; i++)
+{
+    result = result * numA;
+}
 
-//  Console.WriteLine ($"Это число {result}");
+ Console.WriteLine ($"Это число {result}");
 
 
 
@@ -33,20 +33,43 @@
 // 9012 -> 12
 
 
-// Console.WriteLine("Напиши число и я выдам сумму цифр в числе");
-// int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Напиши число и я выдам сумму цифр в числе");
+int number = Convert.ToInt32(Console.ReadLine());
 
 
-// int result = 0;
-// while ( number > 0)
-// {
-//     result = result + number %10;
-//     number = number /10;
-// }
+int result_2 = 0;
+while ( number > 0)
+{
+    result_2 = result_2 + number %10;
+    number = number /10;
+}
 
-// System.Console.WriteLine(result);
+System.Console.WriteLine(result);
+
+// Напишите программу, которая задаёт массив 
+// из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.WriteLine("Начальное значения, для диапазона случайного числа");
+int min = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Конечное значения, для диапазона случайного числа");
+int max = Convert.ToInt32(Console.ReadLine());
 
 
 
+int arrayLenght = 8; 
+
+int [] array = new int [arrayLenght];
+Random random = new Random();
+
+for (int i = 0; i < arrayLenght; i++)
+{ 
+    array[i] = new Random().Next (min, max);
+    Console.WriteLine(array [i]+ " ");
+}
+
+    
 
 
