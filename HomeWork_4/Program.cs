57f2c0f1d099122688aae6aae36d03c1d91e3e28
Samurai <samurai230613@gmail.com>
2023-07-21 15:52:17 +0300

    Second quest

--- HomeWork_4/Program.cs
+++ HomeWork_4/Program.cs
@@ -4,18 +4,49 @@
 // 2, 4 -> 16
 
 
-Console.WriteLine("Введите число которое будут возводить в степень");
-int numA = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число которое будут возводить в степень");
+// int numA = Convert.ToInt32(Console.ReadLine());
 
 
-Console.WriteLine("Введи степень");
-int numB = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введи степень");
+// int numB = Convert.ToInt32(Console.ReadLine());
 
-int result = numA;
+// int result = numA;
 
-for (int i = 1; i < numB; i++)
-{
-    result = result * numA;
-}
+// for (int i = 1; i < numB; i++)
+// {
+//     result = result * numA;
+// }
 
- Console.WriteLine ($"Это число {result}");
+//  Console.WriteLine ($"Это число {result}");
+
+
+
+
+
+
+
+// Задача 27: Напишите программу, которая принимает на вход число
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+// Console.WriteLine("Напиши число и я выдам сумму цифр в числе");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+
+// int result = 0;
+// while ( number > 0)
+// {
+//     result = result + number %10;
+//     number = number /10;
+// }
+
+// System.Console.WriteLine(result);
+
+
+
+
+
