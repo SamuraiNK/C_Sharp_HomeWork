4948874be180bcef012985c90427fc4c8ea0a539
Samurai <samurai230613@gmail.com>
2023-07-31 21:43:33 +0300

    New Commit

--- HomeWork_7/Program.cs
+++ HomeWork_7/Program.cs
@@ -15,13 +15,13 @@
 // int columns = Convert.ToInt32(Console.ReadLine());
 
 // double [,] matrix= new double[rows,columns];
-
+// Random rand = new Random();
 // for (int i = 0; i < matrix.GetLength(0); i++)
 // {
 //     for (int j = 0; j < matrix.GetLength(1); j++)
 //     {
-//         matrix[i,j] = new Random().NextDouble(); 
-//         Console.Write(matrix[i,j]+ " ");
+//         matrix[i,j] = Math.Round(rand.Next(-100,101)*0.1, 1); 
+//         Console.Write("{0,6:F2}", matrix[i,j]+ " ");
 //     }           
 // Console.WriteLine();
 // }
